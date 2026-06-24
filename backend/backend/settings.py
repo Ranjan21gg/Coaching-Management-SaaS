4db282b4fcfbc70bbd1e7b9900648a8aa1d18830
Ranjan Kumar Sahoo <ranjankumarsahoo254@gmail.com>
2026-06-24 22:04:05 +0530

    bug fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -172,7 +172,8 @@ import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3'
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
     )
 }
 
