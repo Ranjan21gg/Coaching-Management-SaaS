7f1887905a114041f69c3adf8430fdd6d5b7485c
Ranjan Kumar Sahoo <ranjankumarsahoo254@gmail.com>
2026-07-03 21:33:45 +0530

    some changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -193,25 +193,14 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise Exception("DATABASE_URL is missing!")
 
-# DATABASES = {
-#     "default": dj_database_url.parse(
-#         DATABASE_URL,
-#         conn_max_age=600,
-#         ssl_require=True
-#     )
-# }
-
-
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.parse(
+        DATABASE_URL,
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
