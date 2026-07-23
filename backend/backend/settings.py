e9b7ac4650c2f027f63612aedd64f5b9eedbad79
Ranjan Kumar Sahoo <ranjankumarsahoo254@gmail.com>
2026-07-23 21:50:04 +0530

    backend setting config

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -92,6 +92,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
+    "https://instiflow-frontend.vercel.app",
     "https://instiflow-three.vercel.app",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
